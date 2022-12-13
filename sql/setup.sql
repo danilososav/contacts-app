f769c98a93441b24809dcfc3239b547081cecfae
danilososav <danilososavazquez@gmail.com>
2022-12-12 21:50:43 -0300

    Create user

--- sql/setup.sql
+++ sql/setup.sql
@@ -3,6 +3,15 @@ DROP DATABASE IF EXISTS contacts_app;
 CREATE DATABASE contacts_app;
 USE contacts_app;
 
+CREATE TABLE users ( 
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(255) UNIQUE,
+    password VARCHAR(255)
+);
+
+insert into users (name,email,password) VALUES ("test","test@test.com", "123");
+
 CREATE TABLE contacts (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
@@ -10,3 +19,5 @@ CREATE TABLE contacts (
 );
 
 INSERT INTO contacts (name,phone_number) VALUES ("PEPE", "123");
+
+
